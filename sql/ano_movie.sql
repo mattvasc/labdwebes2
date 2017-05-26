3b7840d4a2e1461b05e45ac6a558aa9e381d8562
mattvasc <mateus.s.vasconcelos@outlook.com>
2017-05-26 11:38:47 -0300

    blah

--- sql/ano_movie.sql
+++
@@ -1,7 +0,0 @@
-
-
---Removendo ?????
-UPDATE movies_slice SET mvyear = NULL WHERE mvyear = '????' OR mvyear =  '\''';
--- Arrumando anomalias
-SELECT substring( unnest(regexp_matches(title, '\([0-9]{4}\)')) from 2 for 4 )
-  FROM public.movies_slice;
---
+++ sql/fix_movie_year.sql
@@ -0,0 +1,10 @@
+
+
+--Removendo ????? se der errado, teste com '\'''
+UPDATE movies_slice SET mvyear = NULL WHERE mvyear = '????' OR mvyear =  '\'\'';
+-- Arrumando anomalias
+--SELECT substring( unnest(regexp_matches(title, '\([0-9]{4}')) from 2 for 4 )
+  --FROM public.movies_slice;
+
+
+UPDATE movies_slice SET mvyear = substring( unnest(regexp_matches(title, '\([0-9]{4}')) from 2 for 4 ) WHERE mvyear IS NULL OR TRIM(mvyear) =  '';
