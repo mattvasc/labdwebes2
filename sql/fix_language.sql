9d041373ff7e2dbaa440e594cb7de966f2e75b6d
CARLA GAMA <carla.gaama@gmail.com>
2017-05-26 13:58:21 -0300

    nome da tabela para lang ao inves da palavra reservada language

--- sql/fix_language.sql
+++ sql/fix_language.sql
@@ -1,13 +1,15 @@
 CREATE OR REPLACE FUNCTION fix_language(movieid integer, languages varchar(1000)) RETURNS void AS
 $$
 BEGIN
-    INSERT INTO language (movieid, language) VALUES (movieid, TRIM(UNNEST(STRING_TO_ARRAY(languages, ';'))));
+    INSERT INTO lang (movieid, languages) VALUES (movieid, TRIM(UNNEST(STRING_TO_ARRAY(languages, ';'))));
 END
 $$ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION call_fix_language() RETURNS void AS
   $$
   BEGIN
+      CREATE TABLE lang(movieid integer, languages varchar(1000));
+
       PERFORM fix_language( movieid, languages ) FROM (SELECT DISTINCT t.movieid, t.languages FROM movies_slice AS t) AS s; -- is ok
       ALTER TABLE movies_slice DROP COLUMN languages;
   END
