8b24b4f436050327bc208a661b166aa1040fb9a1
CARLA GAMA <carla.gaama@gmail.com>
2017-05-26 14:21:27 -0300

    mudança na query de update, usando LIKE %' para pegas as aspas simples.

--- sql/fix_movie_year.sql
+++ sql/fix_movie_year.sql
@@ -1,7 +1,9 @@
 
 
 --Removendo ????? se der errado, teste com '\'''
-UPDATE movies_slice SET mvyear = NULL WHERE mvyear = '????' OR mvyear =  '\'\'';
+--UPDATE movies_slice SET mvyear = NULL WHERE mvyear = '????' OR mvyear =  
+
+UPDATE movies_slice SET mvyear = NULL WHERE mvyear = '????' OR mvyear like '%''%';
 -- Arrumando anomalias
 --SELECT substring( unnest(regexp_matches(title, '\([0-9]{4}')) from 2 for 4 )
   --FROM public.movies_slice;
