b68631fbf543c423243bc5ac3e933dea1edf4041
mattvasc <mateus.s.vasconcelos@outlook.com>
2017-05-26 10:58:22 -0300

    querie geradora dos slices

---
+++ sql/query_slice.sql
@@ -0,0 +1,44 @@
+
+
+-- Cria cão das tabelas:
+CREATE TABLE public.movies_slice
+(
+  movieid integer,
+  title character varying(400),
+  mvyear character varying(100),
+  actorid integer,
+  actorname character varying(250),
+  sex character(1),
+  as_character character varying(1000),
+  languages character varying(1000),
+  genres character varying(100)
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+CREATE TABLE public.directorsmovies_slice
+(
+  movieid integer,
+  directorid integer,
+  dname character varying(500),
+  addition character varying(1000)
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+-- Povoa mentos das tabelas:
+INSERT INTO public.movies_slice(
+            movieid, title, mvyear, actorid, actorname, sex, as_character,
+            languages, genres)
+SELECT movieid, title, mvyear, actorid, actorname, sex, as_character,
+       languages, genres
+  FROM public.movies LIMIT 500;
+
+  INSERT INTO public.directorsmovies_slice(
+              movieid, directorid, dname, addition)
+    SELECT movieid, directorid, dname, addition
+    FROM public.directorsmovies LIMIT 500;
