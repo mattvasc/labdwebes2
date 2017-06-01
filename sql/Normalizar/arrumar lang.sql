bfa022ab2272e7ef6f47f68142874eff2eb1c923
mattvasc <mateus.s.vasconcelos@outlook.com>
2017-06-01 12:34:23 -0300

    arrumarn lang

---
+++ sql/Normalizar/arrumar lang.sql
@@ -0,0 +1,19 @@
+﻿
+
+
+
+-- DROPAR PK PRIMEIRAMENTE DE LANG
+UPDATE lang SET lang = SUBSTRING(lang FROM '^([\w\-]+)');
+
+
+
+-- Removendo duplicatas:
+DELETE FROM lang WHERE EXISTS
+(SELECT 1 FROM lang l2
+	WHERE l2.movieid = lang.movieid AND
+	l2.lang = lang.lang AND
+	l2.ctid > lang.ctid
+);
+
+ALTER TABLE public.lang
+  ADD PRIMARY KEY (movieid, lang);
