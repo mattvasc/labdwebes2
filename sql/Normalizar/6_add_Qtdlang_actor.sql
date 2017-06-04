09a39d464df2b0e52aab7ee60c42833d29de7775
CARLA GAMA <carla.gaama@gmail.com>
2017-06-04 20:07:17 -0300

    renomeando para n_lang

--- sql/Normalizar/6_add_Qtdlang_actor.sql
+++ sql/Normalizar/6_add_Qtdlang_actor.sql
@@ -7,7 +7,7 @@ $$
         lang l
         USING (movieid)
    GROUP BY id) i WHERE actor.actorid = i.id;
-   UPDATE actor SET lang = 0 WHERE lang IS NULL;
+   UPDATE actor SET n_lang = 0 WHERE n_lang IS NULL;
    END
    $$ LANGUAGE 'plpgsql';
 
