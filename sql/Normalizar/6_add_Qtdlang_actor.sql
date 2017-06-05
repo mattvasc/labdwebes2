a4408a1fbd4fdd8a5471c36e884be2d72b79fe4f
mattvasc <mateus.s.vasconcelos@outlook.com>
2017-06-05 19:28:55 -0300

    arrumada

--- sql/Normalizar/6_add_Qtdlang_actor.sql
+++ sql/Normalizar/6_add_Qtdlang_actor.sql
@@ -1,18 +1,8 @@
-CREATE OR REPLACE FUNCTION call_fix_actor() RETURNS void AS
-$$
-  BEGIN
-  ALTER TABLE actor ADD n_lang integer;
+
   UPDATE actor SET n_lang = i.numlangs FROM (SELECT act.actorid as id, COUNT(DISTINCT l.lang) as numlangs
    FROM act JOIN
         lang l
         USING (movieid)
    GROUP BY id) i WHERE actor.actorid = i.id;
    UPDATE actor SET n_lang = 0 WHERE n_lang IS NULL;
-   END
-   $$ LANGUAGE 'plpgsql';
 
-DO $$
- BEGIN
-  call_fix_actor();
- END;
-$$
