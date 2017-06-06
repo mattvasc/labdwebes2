20ae3cd954eafa27a889c69e4ae69eb5e4b8472f
mattvasc <mateus.s.vasconcelos@outlook.com>
2017-06-06 15:02:23 -0300

    boa

---
+++ sql/Normalizar/fix_actorname.sql
@@ -0,0 +1,3 @@
+﻿
+UPDATE actor SET actorname =  TRIM(SUBSTRING(actorname FROM '.+?(?=\()')) WHERE actorname LIKE '%(%';
+UPDATE actor SET actorname = SUBSTRING(actorname FROM ',\s*(.*)') || ' ' || SUBSTRING(actorname FROM '^([\w\-]+)') WHERE actorname LIKE '%, %';
