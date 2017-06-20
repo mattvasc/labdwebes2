e78e3a004c144e399b89c8d096ae8dc7d29f2bf6
CARLA GAMA <carla.gaama@gmail.com>
2017-06-19 21:02:13 -0300

    SQL UPDATE para traduzir os gêneros

---
+++ sql/Normalizar/translateGenre.sql
@@ -0,0 +1,25 @@
+UPDATE genre SET genre = 'Comédia' WHERE genre = 'Comedy';
+UPDATE genre SET genre = 'Ação' WHERE genre = 'Action';
+UPDATE genre SET genre = 'Adulto' WHERE genre = 'Adult';
+UPDATE genre SET genre = 'Aventura' WHERE genre = 'Adventure';
+UPDATE genre SET genre = 'Animação' WHERE genre = 'Animation';
+UPDATE genre SET genre = 'Biografia' WHERE genre = 'Biography';
+UPDATE genre SET genre = 'Documentário' WHERE genre = 'Documentary';
+UPDATE genre SET genre = 'Família' WHERE genre = 'Family';
+UPDATE genre SET genre = 'Fantasia' WHERE genre = 'Fantasy';
+UPDATE genre SET genre = 'História' WHERE genre = 'History';
+UPDATE genre SET genre = 'Terror' WHERE genre = 'Horror';
+UPDATE genre SET genre = 'Estilo de Vida' WHERE genre = 'Lifestyle';
+UPDATE genre SET genre = 'Música' WHERE genre = 'Music';
+UPDATE genre SET genre = 'Esporte' WHERE genre = 'Sport';
+UPDATE genre SET genre = 'Mistério' WHERE genre = 'Mystery';
+UPDATE genre SET genre = 'Notícia' WHERE genre = 'News';
+UPDATE genre SET genre = 'Curta Metragem' WHERE genre = 'Short';
+UPDATE genre SET genre = 'Guerra' WHERE genre = 'War';
+UPDATE genre SET genre = 'Suspense' WHERE genre = 'Thriller';
+UPDATE genre SET genre = 'Ficção Científica' WHERE genre = 'Sci-Fi';
+UPDATE genre SET genre = 'Faroeste' WHERE genre = 'Western';
+
+
+
+
