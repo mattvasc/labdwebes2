f6bbba54567ea8189e99bf52165c98407ec18170
CARLA GAMA <carla.gaama@gmail.com>
2017-06-25 19:32:08 -0300

    tirando ambiguidade da procedure. genre para mgenero

--- sql/procedures/procedure_select_N_atores_filme_em_comum.sql
+++ sql/procedures/procedure_select_N_atores_filme_em_comum.sql
@@ -1,12 +1,12 @@
-CREATE OR REPLACE FUNCTION select_movie_actors_in_common(genre TEXT, actor_one TEXT, actor_two TEXT, actor_tree TEXT, actor_four TEXT, actor_five TEXT)
-RETURNS TABLE (films TEXT) AS
+CREATE OR REPLACE FUNCTION select_movie_actors_in_common(genero TEXT, actor_one TEXT, actor_two TEXT, actor_tree TEXT, actor_four TEXT, actor_five TEXT)
+RETURNS TABLE (mid int, mtitle character varying (400), year character varying (100), mgenre character varying (100)) AS
 
 $BODY$
 
 BEGIN
 	IF actor_two IS NULL THEN
 			RETURN QUERY (SELECT distinct movieid, title, mvyear, genre
-		  FROM (SELECT movieid, genre FROM genre WHERE genre = genre) AS generos
+		  FROM (SELECT movieid, genre.genre FROM genre WHERE genre.genre = genero) AS generos
 		  NATURAL JOIN (
 		    (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_one) as sub1
 			))AS atores_movieid NATURAL JOIN movie order by title);
