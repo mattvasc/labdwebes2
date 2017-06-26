6beed1fbd3f169a07ff2c13fc7a8213f5742d6d3
Mateus S. Vasconcelos <mateus.s.vasconcelos@outlook.com>
2017-06-26 06:12:07 -0300

    RODAR AVANCADA.SQLgit add sql/!

--- sql/procedures/avancada.sql
+++ sql/procedures/avancada.sql
@@ -1,49 +1,57 @@
-CREATE OR REPLACE FUNCTION select_movie_actors_in_common(genero TEXT, actor_one TEXT, actor_two TEXT, actor_three TEXT, actor_four TEXT, actor_five TEXT)
-RETURNS TABLE (mid int, mtitle character varying (400), year character varying (100), mgenre character varying (100)) AS
 
+DROP FUNCTION IF EXISTS public.select_movie_actors_in_common(text, text, text, text, text, text);
+
+CREATE OR REPLACE FUNCTION public.select_movie_actors_in_common(
+    IN genero text,
+    IN actor_one text,
+    IN actor_two text,
+    IN actor_tree text,
+    IN actor_four text,
+    IN actor_five text)
+  RETURNS TABLE(mid integer, mtitle character varying, year character varying) AS
 $BODY$
 
 BEGIN
 	IF actor_two IS NULL THEN
-			RETURN QUERY (SELECT distinct movieid, title, mvyear, genre
-		  FROM (SELECT movieid, genre.genre FROM genre WHERE genre.genre = genero) AS generos
+			RETURN QUERY (SELECT distinct movieid, title, mvyear
+		  FROM (SELECT movieid FROM genre WHERE genre.genre = genero) AS generos
 		  NATURAL JOIN (
 		    (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_one) as sub1
 			))AS atores_movieid NATURAL JOIN movie order by title);
 
-		ELSIF actor_three IS NULL THEN
-			RETURN QUERY(SELECT distinct movieid, title, mvyear, genre
-			  FROM (SELECT movieid, genre FROM genre WHERE genre = genero) AS generos
+		ELSIF actor_tree IS NULL THEN
+			RETURN QUERY(SELECT distinct movieid, title, mvyear
+			  FROM (SELECT movieid FROM genre WHERE genre = genero) AS generos
 			  NATURAL JOIN (
 			    (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_one) as sub1
 					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_two) as sub2
 				)))AS atores_movieid NATURAL JOIN movie order by title);
 
 		ELSIF actor_four IS NULL THEN
-			RETURN QUERY (SELECT distinct movieid, title, mvyear, genre
-				FROM (SELECT movieid, genre FROM genre WHERE genre = genero) AS generos
+			RETURN QUERY (SELECT distinct movieid, title, mvyear
+				FROM (SELECT movieid FROM genre WHERE genre = genero) AS generos
 				NATURAL JOIN (
 					(SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_one) as sub1
 					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_two) as sub2
-					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_three) as sub3
+					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_tree) as sub3
 				))))AS atores_movieid NATURAL JOIN movie order by title);
 
 		ELSIF actor_five IS NULL THEN
-			RETURN QUERY (SELECT distinct movieid, title, mvyear, genre
-				FROM (SELECT movieid, genre FROM genre WHERE genre = genero) AS generos
+			RETURN QUERY (SELECT distinct movieid, title, mvyear
+				FROM (SELECT movieid FROM genre WHERE genre = genero) AS generos
 				NATURAL JOIN (
 					(SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_one) as sub1
 					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_two) as sub2
-					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_three) as sub3
+					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_tree) as sub3
 					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_four) as sub4
 				)))))AS atores_movieid NATURAL JOIN movie order by title);
 		ELSE
-			RETURN QUERY (SELECT distinct movieid, title, mvyear, genre
-				FROM (SELECT movieid, genre FROM genre WHERE genre = genero) AS generos
+			RETURN QUERY (SELECT distinct movieid, title, mvyear 
+				FROM (SELECT movieid FROM genre WHERE genre = genero) AS generos
 				NATURAL JOIN (
 					(SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_one) as sub1
 					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_two) as sub2
-					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_three) as sub3
+					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_tree) as sub3
 					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_four) as sub4
 					INTERSECT (SELECT movieid FROM act NATURAL JOIN (SELECT actorid FROM actor WHERE actor.actorname = actor_five) as sub5
 				))))))AS atores_movieid NATURAL JOIN movie order by title);
@@ -54,10 +62,9 @@ $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100
   ROWS 1000;
-
-ALTER FUNCTION public.select_movie_actors_in_common(genre TEXT, actor_one TEXT, actor_two TEXT, actor_three TEXT, actor_four TEXT, actor_five TEXT)
-
+ALTER FUNCTION public.select_movie_actors_in_common(text, text, text, text, text, text)
   OWNER TO aluno;
+
 
 
 DROP FUNCTION IF EXISTS additional_fields(INTEGER);
