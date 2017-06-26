14729213e618188df6466b9ec8c41bcb9e96f567
Mateus S. Vasconcelos <mateus.s.vasconcelos@outlook.com>
2017-06-25 23:29:28 -0300

    Nossa mae já pode parar de saber que o Ranking ta fazendo get

--- www/src/java/Controller/Ranking.java
+++ www/src/java/Controller/Ranking.java
@@ -151,7 +151,6 @@ public class Ranking extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                System.out.println("ESTOU FAZENDO GET MAMAE!!!!!!!");
 
         processRequest(request, response);
     }
@@ -167,7 +166,6 @@ public class Ranking extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        System.out.println("ESTOU FAZENDO POST MAMAE!!!!!!!");
         processRequest(request, response);
     }
 
